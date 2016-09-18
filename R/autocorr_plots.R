b3e01b71c9a7545f270323c2c655b745bdf48294
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-09-18 16:30:51 +1000

    ajusted plot parameters

--- R/autocorr_plots.R
+++ R/autocorr_plots.R
@@ -8,25 +8,17 @@ cal.cor.df <- cbind(cal.cor.df,"col"="deer")
 
 auto.resid <- rbind(cal.cor.df, vic.cor.df)
 
+tiff('figs/sac.tif', pointsize = 11, compression = "lzw", res=300, width = 1600, height = 900)
 ggplot(auto.resid,aes(x=x,y=y,group=col,shape=col)) +
-  geom_line(colour=c("grey70"),size=.75) +
-  geom_point(size=2.5) +
+  geom_line(colour=c("grey70"),size=.3) +
+  geom_point(size=1.0) +
   ylab("Moran's I") +
   xlab("Distance (km)") +
   labs(shape = "Species") +
   theme_bw() +
   theme(legend.key = element_blank()) +
   #scale_shape_manual(values=shapes) +
-  geom_hline(aes(yintercept=0), linetype=2) +
+  geom_hline(aes(yintercept=0), linetype=2, size=0.3) +
+  theme(text = element_text(size = 10)) +
   scale_x_continuous(breaks=seq(1, 20, 1))
-
-#Plot first 20 kms in 1 km bins
-ggplot(vic.cor.df,aes(x=x,y=y)) + 
-  geom_line(colour=c("grey70"),size=1) + 
-  ylab("Moran's I") + 
-  xlab("Distance (km)") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) +
-  theme(text = element_text(size = 20)) +
-  geom_hline(aes(yintercept=0), linetype=2) + 
-  scale_x_continuous(breaks=seq(1, 20, 1))
+dev.off()
