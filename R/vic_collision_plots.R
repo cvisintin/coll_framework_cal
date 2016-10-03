25ef02b8a52bc75b775d3b454b4bb2a41c4769bd
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-10-03 15:53:35 +1100

    updated plots to color

--- R/vic_collision_plots.R
+++ R/vic_collision_plots.R
@@ -4,15 +4,17 @@ require(foreach)
 
 load("output/vic_coll_glm_1000")
 
+plotPal <- c("#94d1c7")
+
 invcloglog <- function (x) {1-exp(-exp(x))}
 
 occ <- foreach(i = 1:length(models1000), .combine=rbind) %do% {
   data.frame(x=models1000[[i]][["data"]][["egk"]], y=invcloglog(cbind(1,log(models1000[[i]][["data"]][["egk"]]),mean(log(models1000[[i]][["data"]][["tvol"]])),mean((log(models1000[[i]][["data"]][["tvol"]]))*(log(models1000[[i]][["data"]][["tvol"]]))),mean(log(models1000[[i]][["data"]][["tspd"]]))) %*% models1000[[i]][["coefs"]]), col=rep(i, each=length(models1000[[i]][["data"]][["egk"]])))
 }
 
-tiff('figs/vic_occ.tif', pointsize = 11, compression = "lzw", res=300, width = 900, height = 900)
+tiff('figs/vic_occ.tif', pointsize = 12, compression = "lzw", res=300, width = 900, height = 900)
 ggplot(occ, aes(x=x,y=y,group=col)) +
-  geom_line(size=0.2, col="black", alpha=0.1) +
+  geom_line(size=0.2, col=plotPal, alpha=0.1) +
   #geom_smooth(size=0.8, se=FALSE, col="black", aes(group=1), n=10000) +
   ylab("Likelihood of Collision") +
   xlab("Likelihood of Species Occurrence") +
@@ -23,7 +25,7 @@ ggplot(occ, aes(x=x,y=y,group=col)) +
   theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
   theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
   theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
-  theme(text = element_text(size = 11)) +
+  theme(text = element_text(size = 10)) +
   scale_x_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) +
   scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
   #guides(colour=FALSE)
@@ -34,9 +36,9 @@ tvol <- foreach(i = 1:length(models1000), .combine=rbind) %do% {
   data.frame(x=models1000[[i]][["data"]][["tvol"]], y=invcloglog(cbind(1,mean(log(models1000[[i]][["data"]][["egk"]])),log(models1000[[i]][["data"]][["tvol"]]),(log(models1000[[i]][["data"]][["tvol"]]))*(log(models1000[[i]][["data"]][["tvol"]])),mean(log(models1000[[i]][["data"]][["tspd"]]))) %*% models1000[[i]][["coefs"]]), col=rep(i, each=length(models1000[[i]][["data"]][["tvol"]])))
 }
 
-tiff('figs/vic_tvol.tif', pointsize = 11, compression = "lzw", res=300, width = 900, height = 900)
+tiff('figs/vic_tvol.tif', pointsize = 12, compression = "lzw", res=300, width = 900, height = 900)
 ggplot(tvol, aes(x=x/1000,y=y,group=col)) +
-  geom_line(size=0.2, col="black", alpha=0.1) +
+  geom_line(size=0.2, col=plotPal, alpha=0.1) +
   #geom_smooth(size=0.8, se=FALSE, col="black", aes(group=1), n=10000) +
   ylab("Likelihood of Collision") +
   xlab("Traffic Volume (1000 vehicles/day)") +
@@ -47,7 +49,7 @@ ggplot(tvol, aes(x=x/1000,y=y,group=col)) +
   theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
   theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
   theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
-  theme(text = element_text(size = 11)) +
+  theme(text = element_text(size = 10)) +
   scale_x_continuous(breaks=seq(0,25,by=5), expand = c(0, 0), lim=c(0,25)) +
   scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
   #guides(colour=FALSE)
@@ -58,9 +60,9 @@ tspd <- foreach(i = 1:length(models1000), .combine=rbind) %do% {
   data.frame(x=models1000[[i]][["data"]][["tspd"]], y=invcloglog(cbind(1,mean(log(models1000[[i]][["data"]][["egk"]])),mean(log(models1000[[i]][["data"]][["tvol"]])),mean((log(models1000[[i]][["data"]][["tvol"]]))*(log(models1000[[i]][["data"]][["tvol"]]))),log(models1000[[i]][["data"]][["tspd"]])) %*% models1000[[i]][["coefs"]]), col=rep(i, each=length(models1000[[i]][["data"]][["tspd"]])))
 }
 
-tiff('figs/vic_tspd.tif', pointsize = 11, compression = "lzw", res=300, width = 900, height = 900)
+tiff('figs/vic_tspd.tif', pointsize = 12, compression = "lzw", res=300, width = 900, height = 900)
 ggplot(tspd, aes(x=x,y=y,group=col)) +
-  geom_line(size=0.2, col="black", alpha=0.1) +
+  geom_line(size=0.2, col=plotPal, alpha=0.1) +
   #geom_smooth(size=0.8, se=FALSE, col="black", aes(group=1), n=10000) +
   ylab("Likelihood of Collision") +
   xlab("Traffic Speed (km/hour)") +
@@ -71,7 +73,7 @@ ggplot(tspd, aes(x=x,y=y,group=col)) +
   theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
   theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
   theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
-  theme(text = element_text(size = 11)) +
+  theme(text = element_text(size = 10)) +
   scale_x_continuous(breaks=seq(40,110,by=10), expand = c(0, 0), lim=c(40,110)) +
   scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
   #guides(colour=FALSE)
