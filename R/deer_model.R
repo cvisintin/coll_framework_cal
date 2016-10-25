983638bc30c1bdcfd1275fa3adc948dde08322de
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-10-25 18:11:28 +1100

    added spatial covariates

--- R/deer_model.R
+++ R/deer_model.R
@@ -9,23 +9,23 @@ require(rgdal)
 require(rgeos)
 require(scales)
 
-grid.files <- list.files(path='data/grids/cal/envi') #Create vector of filenames
-grid.names <- substring(unlist(strsplit(grid.files,"\\_1000."))[(1:(2*(length(grid.files)))*2)-1][1:length(grid.files)],18) #Create vector of covariate names
-
-cal.rst.study <- raster("data/grids/cal/CAL_NAD8310_GRID_STUDY_1000.tif")
-#cal.rst.state <- raster("data/grids/cal/CAL_NAD8310_GRID_STATE_1000.tif")
-clip.study <- extent(445000,1165000,3962000,4329000) #Define clipping extent of maps
-#clip.state <- extent(374000,1318000,3613000,4654000)
-
-#Read in grids, crop, and multiply with template to create consistent covariate maps
-for (i in 1:length(grid.files)) {
-  temp <- raster(paste0("data/grids/cal/envi/",grid.files[i]))
-  #temp <- crop(temp, clip.state)
-  #assign(grid.names[i],temp * cal.rst.state)
-  temp <- crop(temp, clip.study)
-  assign(grid.names[i],temp * cal.rst.study)
-}
-vars <- stack(mget(grid.names)) #Combine all maps to single stack
+# grid.files <- list.files(path='data/grids/cal/envi') #Create vector of filenames
+# grid.names <- substring(unlist(strsplit(grid.files,"\\_1000."))[(1:(2*(length(grid.files)))*2)-1][1:length(grid.files)],18) #Create vector of covariate names
+# 
+# cal.rst.study <- raster("data/grids/cal/CAL_NAD8310_GRID_STUDY_1000.tif")
+# #cal.rst.state <- raster("data/grids/cal/CAL_NAD8310_GRID_STATE_1000.tif")
+# clip.study <- extent(445000,1165000,3962000,4329000) #Define clipping extent of maps
+# #clip.state <- extent(374000,1318000,3613000,4654000)
+# 
+# #Read in grids, crop, and multiply with template to create consistent covariate maps
+# for (i in 1:length(grid.files)) {
+#   temp <- raster(paste0("data/grids/cal/envi/",grid.files[i]))
+#   #temp <- crop(temp, clip.state)
+#   #assign(grid.names[i],temp * cal.rst.state)
+#   temp <- crop(temp, clip.study)
+#   assign(grid.names[i],temp * cal.rst.study)
+# }
+# vars <- stack(c(mget(grid.names),"X"=X,"Y"=Y)) #Combine all maps to single stack
 
 #Read in modelling dataset
 model.data <- read.csv("data/cal_model_data_sdm.csv", header=T, sep=",")
@@ -41,8 +41,9 @@ sdm.colors = colorRampPalette(c("white","red"))
 set.seed(123)
 
 #Construct and run boosted regression tree model
-deer.brt = gbm.step(data = model.data, gbm.x = 4:10, gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5)
+deer.brt = gbm.step(data = model.data, gbm.x = 4:12, gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5)
 save(deer.brt,file="output/cal_brt")
+summary(deer.brt)
 
 #Report reduction in deviance on null model (percent of error explained by model)
 brt.devexp <- paste(round(((deer.brt[["self.statistics"]][["mean.null"]] - deer.brt[["cv.statistics"]][["deviance.mean"]])/deer.brt[["self.statistics"]][["mean.null"]])*100,2)," +/- ",round((deer.brt[["cv.statistics"]][["deviance.se"]]/deer.brt[["self.statistics"]][["mean.null"]])*100,2),sep="")
@@ -56,6 +57,7 @@ brt.roc
 #deer.brt.simp = gbm.simplify(deer.brt)
 
 #Make predictions with model fit based on covariate values in maps
+load("data/cal_study_vars")
 brt.preds <- predict(vars, deer.brt, n.trees=deer.brt$gbm.call$best.trees, type="response")
 
 #Write out prediction map (relative likelihood of occurrence) in TIFF format
@@ -66,17 +68,6 @@ plot(brt.preds, col=sdm.colors(100))
 
 #Calculate spatial autocorrelation in model residuals and create dataframe
 cor <- correlog(model.data[,1], model.data[,2], resid(deer.brt), increment=1000, resamp=0, latlon=FALSE)
-cal.cor.df <- data.frame(x=as.numeric(names(cor$correlation[2:20])), y=cor$correlation[2:20])
+cal.cor.df <- data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20])
 
 save(cal.cor.df,file="output/cal_brt_cor")
-
-#Plot first 20 kms in 1 km bins
-ggplot(cal.cor.df,aes(x=x,y=y)) + 
-  geom_line(colour=c("grey70"),size=1) + 
-  ylab("Moran's I") + 
-  xlab("Distance (km)") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) +
-  theme(text = element_text(size = 20)) +
-  geom_hline(aes(yintercept=0), linetype=2) + 
-  scale_x_continuous(breaks=seq(1, 20, 1))
--- R/egk_model.R
+++ R/egk_model.R
@@ -5,22 +5,23 @@ require(raster)
 require(gbm)
 require(dismo)
 require(data.table)
+require(ncf)
 
-grid.files <- list.files(path='data/grids/vic/envi') #Create vector of filenames
-
-grid.names <- substring(unlist(strsplit(grid.files,"\\_1000."))[(1:(2*(length(grid.files)))*2)-1][1:length(grid.files)],18) #Create vector of covariate names
-
-vic.rst <- raster("data/grids/vic/VIC_GDA9455_GRID_STATE_1000.tif")
-
-clip <- extent(-58000, 764000, 5661000, 6224000) #Define clipping extent of maps
-
-#Read in grids, crop, and multiply with template to create consistent covariate maps
-for (i in 1:length(grid.files)) {
-  temp <- raster(paste0("data/grids/vic/envi/",grid.files[i]))
-  temp <- crop(temp, clip)
-  assign(grid.names[i],temp * vic.rst)
-}
-vars <- stack(mget(grid.names)) #Combine all maps to single stack
+# grid.files <- list.files(path='data/grids/vic/envi') #Create vector of filenames
+# 
+# grid.names <- substring(unlist(strsplit(grid.files,"\\_1000."))[(1:(2*(length(grid.files)))*2)-1][1:length(grid.files)],18) #Create vector of covariate names
+# 
+# vic.rst <- raster("data/grids/vic/VIC_GDA9455_GRID_STATE_1000.tif")
+# 
+# clip <- extent(-58000, 764000, 5661000, 6224000) #Define clipping extent of maps
+# 
+# #Read in grids, crop, and multiply with template to create consistent covariate maps
+# for (i in 1:length(grid.files)) {
+#   temp <- raster(paste0("data/grids/vic/envi/",grid.files[i]))
+#   temp <- crop(temp, clip)
+#   assign(grid.names[i],temp * vic.rst)
+# }
+# vars <- stack(c(mget(grid.names),"X"=X,"Y"=Y)) #Combine all maps to single stack
 
 model.data <- read.csv("data/vic_model_data_sdm.csv", header=T, sep=",")
 
@@ -32,8 +33,9 @@ sdm.colors = colorRampPalette(c("white","red")) #Define color scheme for plottin
 
 set.seed(123) #Set random seed to make results of gradient boosted regressions identical for each run
 
-kang.brt = gbm.step(data = model.data, gbm.x = 4:10, gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5) #Create boosted regression tree model
+kang.brt = gbm.step(data = model.data, gbm.x = 4:12, gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5) #Create boosted regression tree model
 save(kang.brt,file="output/vic_brt")
+summary(kang.brt)
 
 #Report reduction in deviance on null model (percent of error explained by model)
 brt.devexp <- paste(round(((kang.brt[["self.statistics"]][["mean.null"]] - kang.brt[["cv.statistics"]][["deviance.mean"]])/kang.brt[["self.statistics"]][["mean.null"]])*100,2)," +/- ",round((kang.brt[["cv.statistics"]][["deviance.se"]]/kang.brt[["self.statistics"]][["mean.null"]])*100,2),sep="")
@@ -43,17 +45,18 @@ brt.devexp
 brt.roc <- paste(round(kang.brt[["cv.statistics"]][["discrimination.mean"]],2)," +/- ",round(kang.brt[["cv.statistics"]][["discrimination.se"]],2),sep="")
 brt.roc
 
+load("data/vic_study_vars")
 brt.preds <- predict(vars, kang.brt, n.trees=kang.brt$gbm.call$best.trees, type="response") #Make predictions with model fit based on covariate values in maps
 
 writeRaster(brt.preds, filename="output/egk_preds_brt.tif", format="GTiff", overwrite=TRUE) #Write out prediction map in tif format
 
-#Use system to translate and uplaod grid to postgis database
+#Use system commands to translate and uplaod grid to postgis database
 #system("raster2pgsql -d -I -M -s 28355 -t auto /home/casey/Research/Github/coll_framework_egk/output/egk_preds_brt.tif gis_victoria.vic_gda9455_grid_egk_preds_brt | PGPASSWORD=Qpostgres15 psql -d qaeco_spatial -h boab.qaeco.com -p 5432 -U qaeco -w")
 
 plot(brt.preds, col=sdm.colors(100)) #Plot prediction map using red to white color scheme
 
 #Calculate spatial autocorrelation in model residuals and create dataframe
 cor <- correlog(model.data[,1], model.data[,2], resid(kang.brt), increment=1000, resamp=0, latlon=FALSE)
-vic.cor.df <- data.frame(x=as.numeric(names(cor$correlation[2:20])), y=cor$correlation[2:20])
+vic.cor.df <- data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20])
 
 save(vic.cor.df,file="output/vic_brt_cor")
