28e1a23d7a6616dfa1b578ff176b988cf89ba917
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-10-25 18:10:24 +1100

    added calculations for quantile residuals

--- R/cal_collision.R
+++ R/cal_collision.R
@@ -3,6 +3,7 @@ require(data.table)
 require(raster)
 require(boot)
 require(doMC)
+require(fields)
 
 drv <- dbDriver("PostgreSQL")  #Specify a driver for postgreSQL type database
 con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres15", host="boab.qaeco.com", port="5432")  #Connection to database server on Boab
@@ -40,6 +41,8 @@ sdm.preds <- raster("output/deer_preds_brt.tif")
 
 cov.data$deer <- raster::extract(sdm.preds,cov.data[,.(x,y)])
 
+cov.data$coll <- as.integer(0)
+
 coll <- as.data.table(dbGetQuery(con,"
   SELECT DISTINCT ON (p.id)
     r.uid AS uid, CAST(1 AS INTEGER) AS coll
@@ -58,91 +61,143 @@ coll <- as.data.table(dbGetQuery(con,"
   "))
 setkey(coll,uid)
 
-data1 <- merge(cov.data, coll)
+data1 <- coll
 
-set.seed(123)
+data <- cov.data
+data[data1, coll := i.coll]
+data <- na.omit(data)
+data <- data[!duplicated(data[,.(x,y)]),]
 
-##########Added code for 1000 simulations and model averaging#############
-rns <- sample(1:10000, 1000, replace=F)
-
-models1000 <- foreach(i = 1:length(rns)) %do% {
-  set.seed(rns[i])
-  data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
-  model.data <- rbind(data1,data0)
-  model.data <- na.omit(model.data)
-  coll.glm <- glm(formula = coll ~ log(deer) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
-  list("coefs"=coef(summary(coll.glm))[, "Estimate"],
-       "coefs_se"=coef(summary(coll.glm))[, "Std. Error"],
-       "zvalue"=coef(summary(coll.glm))[, "z value"],
-       "coefs_prz"=coef(summary(coll.glm))[, "Pr(>|z|)"],
-       "devred"=round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),
-       "rocvalue"=roc(model.data$coll,coll.glm$fitted.values),
-       "data"=model.data,
-       "nbg"=length(model.data$coll[model.data$coll==0])
-  )
-}
-save(models1000,file="output/cal_coll_glm_1000")
-
-mean(sapply(models1000, function(x){x[["nbg"]]}))
-sd(sapply(models1000, function(x){x[["nbg"]]}))
-
-mean(sapply(models1000, function(x){x[["devred"]]}))
-sd(sapply(models1000, function(x){x[["devred"]]}))
-
-mean(sapply(models1000, function(x){x[["rocvalue"]]}))
-sd(sapply(models1000, function(x){x[["rocvalue"]]}))
-
-summary1000 <- matrix(NA, nrow=5, ncol=4)
-
-for(i in 1:nrow(summary1000)){
-  summary1000[i,1] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs"]][i]})),2), ")")
-  summary1000[i,2] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs_se"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_se"]][i]})),2), ")")
-  summary1000[i,3] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["zvalue"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["zvalue"]][i]})),2), ")")
-  summary1000[i,4] <- paste0(signif(mean(sapply(models1000, function(x){x[["coefs_prz"]][i]})),3))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_prz"]][i]})),2), ")")
-}
-write.csv(summary1000, file = "output/cal_coll_glm_fit.csv", row.names=FALSE)
-
-preds1000 <- matrix(0, nrow=length(cov.data$uid), ncol=length(rns)+1)
-preds1000[,1] <- cov.data$uid
-for(i in 1:length(rns)){
-  set.seed(rns[i])
-  data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
-  model.data <- rbind(data1,data0)
-  model.data <- na.omit(model.data)
-  coll.glm <- glm(formula = coll ~ log(deer) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
-  preds1000[,i+1] <- predict(coll.glm, cov.data, type="response")
-}
-
-coll.preds.df <- data.frame("uid"=cov.data$uid,"collrisk"=rowMeans(preds1000[,2:1001], na.rm = FALSE))
-
-#write.csv(coll.preds.df, file = "output/cal_coll_preds_glm.csv", row.names=FALSE)
-
-dbWriteTable(con, c("gis_california", "cal_nogeom_roads_deercollrisk_m"), value = coll.preds.df, row.names=FALSE, overwrite=TRUE)
-
+# ##########Added code for 1000 simulations and model averaging#############
+# set.seed(123)
+# rns <- sample(1:10000, 1000, replace=F)
+# 
+# models1000 <- foreach(i = 1:length(rns)) %do% {
+#   set.seed(rns[i])
+#   data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
+#   model.data <- rbind(data1,data0)
+#   model.data <- na.omit(model.data)
+#   coll.glm <- glm(formula = coll ~ log(deer) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
+#   list("coefs"=coef(summary(coll.glm))[, "Estimate"],
+#        "coefs_se"=coef(summary(coll.glm))[, "Std. Error"],
+#        "zvalue"=coef(summary(coll.glm))[, "z value"],
+#        "coefs_prz"=coef(summary(coll.glm))[, "Pr(>|z|)"],
+#        "devred"=round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),
+#        "rocvalue"=roc(model.data$coll,coll.glm$fitted.values),
+#        "data"=model.data,
+#        "nbg"=length(model.data$coll[model.data$coll==0])
+#   )
+# }
+# save(models1000,file="output/cal_coll_glm_1000")
+# 
+# mean(sapply(models1000, function(x){x[["nbg"]]}))
+# sd(sapply(models1000, function(x){x[["nbg"]]}))
+# 
+# mean(sapply(models1000, function(x){x[["devred"]]}))
+# sd(sapply(models1000, function(x){x[["devred"]]}))
+# 
+# mean(sapply(models1000, function(x){x[["rocvalue"]]}))
+# sd(sapply(models1000, function(x){x[["rocvalue"]]}))
+# 
+# summary1000 <- matrix(NA, nrow=5, ncol=4)
+# 
+# for(i in 1:nrow(summary1000)){
+#   summary1000[i,1] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs"]][i]})),2), ")")
+#   summary1000[i,2] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs_se"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_se"]][i]})),2), ")")
+#   summary1000[i,3] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["zvalue"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["zvalue"]][i]})),2), ")")
+#   summary1000[i,4] <- paste0(signif(mean(sapply(models1000, function(x){x[["coefs_prz"]][i]})),3))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_prz"]][i]})),2), ")")
+# }
+# write.csv(summary1000, file = "output/cal_coll_glm_fit.csv", row.names=FALSE)
+# 
+# preds1000 <- matrix(0, nrow=length(cov.data$uid), ncol=length(rns)+1)
+# preds1000[,1] <- cov.data$uid
+# for(i in 1:length(rns)){
+#   set.seed(rns[i])
+#   data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
+#   model.data <- rbind(data1,data0)
+#   model.data <- na.omit(model.data)
+#   coll.glm <- glm(formula = coll ~ log(deer) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
+#   preds1000[,i+1] <- predict(coll.glm, cov.data, type="response")
+# }
+# 
+# coll.preds.df <- data.frame("uid"=cov.data$uid,"collrisk"=rowMeans(preds1000[,2:1001], na.rm = FALSE))
+# 
+# #write.csv(coll.preds.df, file = "output/cal_coll_preds_glm.csv", row.names=FALSE)
+# 
+# dbWriteTable(con, c("gis_california", "cal_nogeom_roads_deercollrisk_m"), value = coll.preds.df, row.names=FALSE, overwrite=TRUE)
+# 
 #########################################################################
 
-# data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
-# 
-# model.data <- rbind(data1,data0)
-# model.data <- na.omit(model.data)
-# 
-# coll.glm <- glm(formula = coll ~ log(deer) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)  #Fit regression model
-# 
-# summary(coll.glm)  #Examine fit of regression model
-# 
-# paste("% Deviance Explained: ",round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),sep="")  #Report reduction in deviance
-# 
-# write.csv(signif(summary(coll.glm)$coefficients, digits=4),"output/cal_coll_coef.csv",row.names=FALSE)
-# 
-# write.csv(formatC(anova(coll.glm)[2:4,2]/sum(anova(coll.glm)[2:4,2]), format='f',digits=4),"output/cal_coll_anova.csv",row.names=FALSE)
-# 
-# save(coll.glm,file="output/cal_coll_glm")
-# 
-# save(model.data,file="output/cal_coll_model_data")
-# 
-# coll.preds <- predict(coll.glm, cov.data, type="response")
-# 
-# coll.preds.df <- as.data.table(cbind("uid"=cov.data$uid,"collrisk"=coll.preds)) #Combine predictions with unique IDs for all road segments
-# coll.preds.df <- na.omit(coll.preds.df)
-# 
-# write.csv(coll.preds.df, file = "output/cal_coll_preds_glm.csv", row.names=FALSE)
+coll.glm <- glm(formula = coll ~ log(deer) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = data)  #Fit regression model
+
+summary(coll.glm)  #Examine fit of regression model
+
+paste("% Deviance Explained: ",round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),sep="")  #Report reduction in deviance
+
+write.csv(signif(summary(coll.glm)$coefficients, digits=4),"output/cal_coll_coef.csv",row.names=FALSE)
+
+write.csv(formatC(anova(coll.glm)[2:4,2]/sum(anova(coll.glm)[2:4,2]), format='f',digits=4),"output/cal_coll_anova.csv",row.names=FALSE)
+
+save(coll.glm,file="output/cal_coll_glm")
+
+save(data,file="output/cal_coll_model_data")
+
+coll.preds <- predict(coll.glm, cov.data, type="response")
+
+coll.preds.df <- as.data.table(cbind("uid"=cov.data$uid,"collrisk"=coll.preds)) #Combine predictions with unique IDs for all road segments
+coll.preds.df <- na.omit(coll.preds.df)
+
+write.csv(coll.preds.df, file = "output/cal_coll_preds_glm.csv", row.names=FALSE)
+
+
+prob <- predict(coll.glm, type = 'response')
+n <- 5000
+
+registerDoMC(cores=detectCores()-1)
+
+system.time(
+coll.resid <- foreach(i = 1:nrow(data), .combine=c) %dopar% {
+  simulations.c = c()
+  while (length(simulations.c) < 10000 &
+         all(simulations.c != coll.glm$y[i])) {
+    set.seed(123+i)
+    simulations.c = c(simulations.c, rbinom(n, 1, prob[i]))
+  }
+  if (!any(simulations.c == coll.glm$y[i]))
+    warning(sprintf('datapoint %i had no valid samples', i))
+  
+  # add jitter
+  set.seed(123+i)
+  fuzzy.y <- coll.glm$y[i] + runif(1, -0.5, 0.5)
+  set.seed(123+i)
+  fuzzy.simulations <- simulations.c + runif(n, -0.5, 0.5)
+  
+  # make sure ecdf doesn't go to 1 or 0
+  sim.limits <- range(sort(unique(fuzzy.simulations))[-c(1,length(unique(fuzzy.simulations)))])
+  fuzzy.y <- pmin(pmax(fuzzy.y, sim.limits[1]), sim.limits[2])
+  
+  ecdf(fuzzy.simulations)(fuzzy.y)
+}
+) ###644 second runtime
+
+save(coll.resid,file="output/cal_coll_resid")
+
+coll.resid.norm <- qnorm(coll.resid)
+
+spc <- cbind(data$x, data$y, coll.resid.norm)
+
+cal.cor.df.1000 <- foreach(i = 1:20, .combine=rbind) %dopar% {
+  set.seed(123+i)
+  spc.r <- spc[sample(nrow(spc), 5000),]
+  cor <- correlog(spc.r[,1], spc.r[,2], spc.r[,3], increment=1000, resamp=0, latlon=FALSE)
+  data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20], n=i)
+}
+save(cal.cor.df.1000,file="output/vic_coll_cor_1000")
+
+cal.cor.df.250 <- foreach(i = 1:20, .combine=rbind) %dopar% {
+  set.seed(123+i)
+  spc.r <- spc[sample(nrow(spc), 5000),]
+  cor <- correlog(spc.r[,1], spc.r[,2], spc.r[,3], increment=250, resamp=0, latlon=FALSE)
+  data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20], n=i)
+}
+save(cal.cor.df.250,file="output/vic_coll_cor_250")
--- R/vic_collision.R
+++ R/vic_collision.R
@@ -3,6 +3,11 @@ require(data.table)
 require(raster)
 require(boot)
 require(doMC)
+require(fields)
+require(spatstat)
+require(maptools)
+require(ncf)
+
 
 drv <- dbDriver("PostgreSQL")  #Specify a driver for postgreSQL type database
 con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres15", host="boab.qaeco.com", port="5432")  #Connection to database server on Boab
@@ -39,6 +44,8 @@ cov.data <- Reduce(function(x, y) merge(x, y, all=TRUE), list(roads,tvol.preds,t
 sdm.preds <- raster("output/egk_preds_brt.tif")
 
 cov.data$egk <- raster::extract(sdm.preds,cov.data[,.(x,y)])
+
+cov.data$coll <- as.integer(0)
 
 # coll <- as.data.table(dbGetQuery(con,"
 #   SELECT DISTINCT ON (p.id)
@@ -163,92 +170,261 @@ setkey(coll,uid)
 #   "))
 # setkey(coll,uid)
 
-data1 <- merge(cov.data, coll)
+data1 <- coll
 
-set.seed(123)
+data <- cov.data
+data[data1, coll := i.coll]
+data <- na.omit(data)
+data <- data[!duplicated(data[,.(x,y)]),]
+
+# vic.rst <- raster("data/grids/vic/VIC_GDA9455_GRID_STATE_1000.tif")
+# 
+# vic.mat <- as.matrix(vic.rst)
+# vic.mat <- vic.mat[nrow(vic.mat):1, ]
+# vic.mat[!is.finite(vic.mat)] <- 0
+# vic.mat2 <- matrix(FALSE,nrow=563,ncol=822)
+# vic.mat2[] <- as.logical(vic.mat)
+# 
+# vic.win <- owin(xrange=c(-58,764), yrange=c(5661,6224), units=c("kilometer","kilometers"), mask=vic.mat2)
+# 
+# vic.ppp <- ppp(data[,x]/1000,data[,y]/1000, window=vic.win, marks=data[,coll])
+# 
+# #K <- Kest(vic.ppp)
+# 
+# vic.idw <- idw(vic.ppp, power=1)
+# rast_idw <- raster(as.SpatialGridDataFrame.im(vic.idw))
+# rast_idw <- setExtent(rast_idw, extent(-58, 764, 5661, 6224))
+# proj4string(rast_idw) <- CRS("+init=epsg:28355")
+# AC <- extract(rast_idw, cbind(data[,x]/1000, data[,y]/1000))
+# 
+# model.data <- cbind(data,AC)
+
+#s <- surf.ls(2, data[,.("x"=x/1000,"y"=y/1000,"z"=coll)])
+#s <- surf.gls(2, expcov, data[,.("x"=x/1000,"y"=y/1000,"z"=coll)], 1, d=1)
+#trsurf <- trmat(s, -58, 764, 5661, 6224, 1)
 
 ##########Added code for 1000 simulations and model averaging#############
-rns <- sample(1:10000, 1000, replace=F)
-
-models1000 <- foreach(i = 1:length(rns)) %do% {
-  set.seed(rns[i])
-  data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
-  model.data <- rbind(data1,data0)
-  model.data <- na.omit(model.data)
-  coll.glm <- glm(formula = coll ~ log(egk) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
-  list("coefs"=coef(summary(coll.glm))[, "Estimate"],
-       "coefs_se"=coef(summary(coll.glm))[, "Std. Error"],
-       "zvalue"=coef(summary(coll.glm))[, "z value"],
-       "coefs_prz"=coef(summary(coll.glm))[, "Pr(>|z|)"],
-       "devred"=round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),
-       "rocvalue"=roc(model.data$coll,coll.glm$fitted.values),
-       "data"=model.data,
-       "nbg"=length(model.data$coll[model.data$coll==0])
-  )
-}
-save(models1000,file="output/vic_coll_glm_1000")
-
-mean(sapply(models1000, function(x){x[["nbg"]]}))
-sd(sapply(models1000, function(x){x[["nbg"]]}))
-
-mean(sapply(models1000, function(x){x[["devred"]]}))
-sd(sapply(models1000, function(x){x[["devred"]]}))
-
-mean(sapply(models1000, function(x){x[["rocvalue"]]}))
-sd(sapply(models1000, function(x){x[["rocvalue"]]}))
-
-summary1000 <- matrix(NA, nrow=5, ncol=4)
-
-for(i in 1:nrow(summary1000)){
-  summary1000[i,1] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs"]][i]})),2), ")")
-  summary1000[i,2] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs_se"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_se"]][i]})),2), ")")
-  summary1000[i,3] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["zvalue"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["zvalue"]][i]})),2), ")")
-  summary1000[i,4] <- paste0(signif(mean(sapply(models1000, function(x){x[["coefs_prz"]][i]})),3))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_prz"]][i]})),2), ")")
-}
-write.csv(summary1000, file = "output/vic_coll_glm_fit.csv", row.names=FALSE)
-
-preds1000 <- matrix(0, nrow=length(cov.data$uid), ncol=length(rns)+1)
-preds1000[,1] <- cov.data$uid
-for(i in 1:length(rns)){
-  set.seed(rns[i])
-  data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
-  model.data <- rbind(data1,data0)
-  model.data <- na.omit(model.data)
-  coll.glm <- glm(formula = coll ~ log(egk) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
-  preds1000[,i+1] <- predict(coll.glm, cov.data, type="response")
-}
-
-coll.preds.df <- data.frame("uid"=cov.data$uid,"collrisk"=rowMeans(preds1000[,2:1001], na.rm = FALSE))
-
-#write.csv(coll.preds.df, file = "output/vic_coll_preds_glm.csv", row.names=FALSE)
-
-dbWriteTable(con, c("gis_victoria", "vic_nogeom_roads_egkcollrisk_m"), value = coll.preds.df, row.names=FALSE, overwrite=TRUE)
+# set.seed(123)
+# rns <- sample(1:10000, 1000, replace=F)
+# 
+# models1000 <- foreach(i = 1:length(rns)) %do% {
+#   set.seed(rns[i])
+#   data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
+#   model.data <- rbind(data1,data0)
+#   model.data <- na.omit(model.data)
+#   coll.glm <- glm(formula = coll ~ log(egk) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
+#   list("coefs"=coef(summary(coll.glm))[, "Estimate"],
+#        "coefs_se"=coef(summary(coll.glm))[, "Std. Error"],
+#        "zvalue"=coef(summary(coll.glm))[, "z value"],
+#        "coefs_prz"=coef(summary(coll.glm))[, "Pr(>|z|)"],
+#        "devred"=round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),
+#        "rocvalue"=roc(model.data$coll,coll.glm$fitted.values),
+#        "data"=model.data,
+#        "nbg"=length(model.data$coll[model.data$coll==0])
+#   )
+# }
+# save(models1000,file="output/vic_coll_glm_1000")
+# 
+# mean(sapply(models1000, function(x){x[["nbg"]]}))
+# sd(sapply(models1000, function(x){x[["nbg"]]}))
+# 
+# mean(sapply(models1000, function(x){x[["devred"]]}))
+# sd(sapply(models1000, function(x){x[["devred"]]}))
+# 
+# mean(sapply(models1000, function(x){x[["rocvalue"]]}))
+# sd(sapply(models1000, function(x){x[["rocvalue"]]}))
+# 
+# summary1000 <- matrix(NA, nrow=5, ncol=4)
+# 
+# for(i in 1:nrow(summary1000)){
+#   summary1000[i,1] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs"]][i]})),2), ")")
+#   summary1000[i,2] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["coefs_se"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_se"]][i]})),2), ")")
+#   summary1000[i,3] <- paste0(formatC(signif(mean(sapply(models1000, function(x){x[["zvalue"]][i]})),digits=3), digits=3, format="fg", flag="#"))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["zvalue"]][i]})),2), ")")
+#   summary1000[i,4] <- paste0(signif(mean(sapply(models1000, function(x){x[["coefs_prz"]][i]})),3))#, " (s.d. ", round(sd(sapply(models1000, function(x){x[["coefs_prz"]][i]})),2), ")")
+# }
+# write.csv(summary1000, file = "output/vic_coll_glm_fit.csv", row.names=FALSE)
+# 
+# preds1000 <- matrix(0, nrow=length(cov.data$uid), ncol=length(rns)+1)
+# preds1000[,1] <- cov.data$uid
+# for(i in 1:length(rns)){
+#   set.seed(rns[i])
+#   data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
+#   model.data <- rbind(data1,data0)
+#   model.data <- na.omit(model.data)
+#   coll.glm <- glm(formula = coll ~ log(egk) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)
+#   preds1000[,i+1] <- predict(coll.glm, cov.data, type="response")
+# }
+# 
+# coll.preds.df <- data.frame("uid"=cov.data$uid,"collrisk"=rowMeans(preds1000[,2:1001], na.rm = FALSE))
+# 
+# #write.csv(coll.preds.df, file = "output/vic_coll_preds_glm.csv", row.names=FALSE)
+# 
+# dbWriteTable(con, c("gis_victoria", "vic_nogeom_roads_egkcollrisk_m"), value = coll.preds.df, row.names=FALSE, overwrite=TRUE)
 
 #########################################################################
 
-# data0 <- cbind(cov.data[sample(seq(1:nrow(cov.data)),2*nrow(data1)),],"coll"=rep(0,2*nrow(data1)))
+coll.glm <- glm(formula = coll ~ log(egk) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = data)  #Fit regression model
+
+summary(coll.glm)  #Examine fit of regression model
+
+paste("% Deviance Explained: ",round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),sep="")  #Report reduction in deviance
+
+# crds <- as.matrix(data[,.("x"=x/1000,"y"=y/1000)])
+# k1 <- knn2nb(knearneigh(crds, 1, longlat=FALSE, RANN=TRUE))
+# #k1d <- nbdists(k1, crds, longlat=FALSE)
+# #max(unlist(k1d))
 # 
-# model.data <- rbind(data1,data0)
-# model.data <- na.omit(model.data)
+# sdist <- 600
+# all.dists <- foreach(i = 1:10000, .combine=c) %do% {
+#   crd <- data[i,c(x,y)]
+#   crds <- as.matrix(data[x<crd[1]+sdist & x>crd[1]-sdist & y<crd[2]+sdist & y>crd[2]-sdist,.(x,y)])
+#   unlist(nbdists(knn2nb(knearneigh(crds, 1, longlat=FALSE)), crds, longlat=FALSE))
+# }
 # 
-# coll.glm <- glm(formula = coll ~ log(egk) + log(tvol) + I(log(tvol)^2) + log(tspd), family=binomial(link = "cloglog"), data = model.data)  #Fit regression model
+# system.time(dnb <- dnearneigh(crds, 0, 1000, longlat=FALSE))
 # 
-# summary(coll.glm)  #Examine fit of regression model
+# out <- RANN::nn2(crds, k=2, treetype="bd", searchtype="radius", radius=21000)$nn.idx[, -1, drop = FALSE]
+# dimnames(out) <- NULL
+# res <- list(nn = out, np = nrow(crds), k = 1, dimension = ncol(crds), x = crds)
+# class(res) <- "knn"
+# attr(res, "call") <- match.call()
 # 
-# paste("% Deviance Explained: ",round(((coll.glm$null.deviance - coll.glm$deviance)/coll.glm$null.deviance)*100,2),sep="")  #Report reduction in deviance
+# #system.time(lw <- nb2listw(knn2nb(res), zero.policy=TRUE))
+# #system.time(mt <- moran.test(resid(coll.glm), lw, zero.policy=TRUE))
 # 
-# write.csv(signif(summary(coll.glm)$coefficients, digits=4),"output/vic_coll_coef.csv",row.names=FALSE)
+# sp.cor <- sp.correlogram(knn2nb(res), resid(coll.glm), order=21, method="I", randomisation=FALSE)
 # 
-# write.csv(formatC(anova(coll.glm)[2:4,2]/sum(anova(coll.glm)[2:4,2]), format='f',digits=4),"output/vic_coll_anova.csv",row.names=FALSE)
-# 
-# save(coll.glm,file="output/vic_coll_glm")
-# 
-# save(model.data,file="output/vic_coll_model_data")
+# #cor <- correlog(data[,.(x,y)], resid(coll.glm), nbclass=21, method="Moran")
+# #cor <- eco.correlog(resid(coll.glm), data[,.(x,y)], int=1000, smax=20000, nclass=20, method="I")
+# #vic.cor.df <- data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20])
+
+write.csv(signif(summary(coll.glm)$coefficients, digits=4),"output/vic_coll_coef.csv",row.names=FALSE)
+
+write.csv(formatC(anova(coll.glm)[2:4,2]/sum(anova(coll.glm)[2:4,2]), format='f',digits=4),"output/vic_coll_anova.csv",row.names=FALSE)
+
+save(coll.glm,file="output/vic_coll_glm")
+
+save(data,file="output/vic_coll_model_data")
+
+coll.preds <- predict(coll.glm, cov.data, type="response")
+
+coll.preds.df <- as.data.table(cbind("uid"=cov.data$uid,"collrisk"=coll.preds)) #Combine predictions with unique IDs for all road segments
+coll.preds.df <- na.omit(coll.preds.df)
+
+write.csv(coll.preds.df, file = "output/vic_coll_preds_glm.csv", row.names=FALSE)
 
 
-# write.csv(coll.preds.df, file = "output/vic_coll_preds_glm.csv", row.names=FALSE)
+######################Stan###################
+# require(rstan)
+# set.seed(123)
+# 
+# #N <- nrow(data)
+# N <- 10000
+# id <- sample(data$uid,10000)
+# y <- sample(data$coll,10000)
+# x1 <- sample(data$egk,10000)
+# x2 <- sample(data$tvol,10000)
+# x3 <- sample(data$tspd,10000)
+# 
+# scode <- "
+#   data{
+#     int<lower=1> N;
+#     int<lower=0,upper=N> id[N];
+#     int<lower=0,upper=1> y[N];
+#     real x1[N];
+#     real x2[N];
+#     real x3[N];
+#   }
+# 
+#   transformed data{
+#     real x1l[N];
+#     real x2l[N];
+#     real x2l2[N];
+#     real x3l[N];
+# 
+#     for (n in 1:N) {
+#       x1l[n] = log(x1[n]);
+#       x2l[n] = log(x2[n]);
+#       x2l2[n] = log(x2[n])*log(x2[n]);
+#       x3l[n] = log(x3[n]);
+#     }
+#   }
+# 
+#   parameters{
+#     vector[N] a;
+#     vector[4] b;
+#     real<lower=0,upper=100> sigma_a;
+#     real mu_a;
+#   }
+# 
+#   transformed parameters {
+#     vector[N] p;
+#     vector[N] lp;
+# 
+#     for (i in 1:N)
+#       lp[i] = a + b[1] * x1l[i] + b[2] * x2l[i] + b[3] * x2l2[i] + b[4] * x3l[i] + a[id[i]];
+#       p[i] = inv_cloglog(lp[i])
+#   } 
+# 
+#   model {
+#     mu_a ~ normal(0, 1);
+#     a ~ normal (mu_a, sigma_a);
+#     b ~ normal (0, 100);
+#     y ~ bernoulli_logit(p);
+#   }
+# "
+# 
+# coll_model_fit <- stan(model_code = scode, iter = 500, chains = 3, cores = 3, seed=123, verbose = FALSE, control =list(stepsize=0.01, adapt_delta=0.9, max_treedepth=15))
+# precis(coll_model_fit)
 
+prob <- predict(coll.glm, type = 'response')
+n <- 5000
 
+registerDoMC(cores=detectCores()-1)
 
+system.time(
+  coll.resid <- foreach(i = 1:nrow(data), .combine=c) %dopar% {
+    simulations.c = c()
+    while (length(simulations.c) < 10000 &
+           all(simulations.c != coll.glm$y[i])) {
+      set.seed(123+i)
+      simulations.c = c(simulations.c, rbinom(n, 1, prob[i]))
+    }
+    if (!any(simulations.c == coll.glm$y[i]))
+      warning(sprintf('datapoint %i had no valid samples', i))
+    
+    # add jitter
+    set.seed(123+i)
+    fuzzy.y <- coll.glm$y[i] + runif(1, -0.5, 0.5)
+    set.seed(123+i)
+    fuzzy.simulations <- simulations.c + runif(n, -0.5, 0.5)
+    
+    # make sure ecdf doesn't go to 1 or 0
+    sim.limits <- range(sort(unique(fuzzy.simulations))[-c(1,length(unique(fuzzy.simulations)))])
+    fuzzy.y <- pmin(pmax(fuzzy.y, sim.limits[1]), sim.limits[2])
+    
+    ecdf(fuzzy.simulations)(fuzzy.y)
+  }
+) ###753 second runtime
 
+save(coll.resid,file="output/vic_coll_resid")
 
+coll.resid.norm <- qnorm(coll.resid)
+
+spc <- cbind(data$x, data$y, coll.resid.norm)
+
+vic.cor.df.1000 <- foreach(i = 1:20, .combine=rbind) %dopar% {
+  set.seed(123+i)
+  spc.r <- spc[sample(nrow(spc), 5000),]
+  cor <- correlog(spc.r[,1], spc.r[,2], spc.r[,3], increment=1000, resamp=0, latlon=FALSE)
+  data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20], n=i)
+}
+save(vic.cor.df.1000,file="output/vic_coll_cor_1000")
+
+vic.cor.df.250 <- foreach(i = 1:20, .combine=rbind) %dopar% {
+  set.seed(123+i)
+  spc.r <- spc[sample(nrow(spc), 5000),]
+  cor <- correlog(spc.r[,1], spc.r[,2], spc.r[,3], increment=250, resamp=0, latlon=FALSE)
+  data.frame(x=as.numeric(names(cor$correlation[1:20])), y=cor$correlation[1:20], n=i)
+}
+save(vic.cor.df.250,file="output/vic_coll_cor_250")
