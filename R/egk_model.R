3b3ca13b00d368ab02a82fdd01f7e2d78436027b
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-10-31 08:32:33 +1100

    revised syntax to upload raster to postGIS server

--- R/egk_model.R
+++ R/egk_model.R
@@ -51,7 +51,7 @@ brt.preds <- predict(vars, kang.brt, n.trees=kang.brt$gbm.call$best.trees, type=
 writeRaster(brt.preds, filename="output/egk_preds_brt.tif", format="GTiff", overwrite=TRUE) #Write out prediction map in tif format
 
 #Use system commands to translate and uplaod grid to postgis database
-#system("raster2pgsql -d -I -M -s 28355 -t auto /home/casey/Research/Github/coll_framework_egk/output/egk_preds_brt.tif gis_victoria.vic_gda9455_grid_egk_preds_brt | PGPASSWORD=Qpostgres15 psql -d qaeco_spatial -h boab.qaeco.com -p 5432 -U qaeco -w")
+#system("raster2pgsql -d -C -I -M -s 28355 -t auto /home/casey/Research/Github/coll_framework_cal/output/egk_preds_brt.tif gis_victoria.vic_gda9455_grid_egk_preds_brt | PGPASSWORD=Qpostgres15 psql -d qaeco_spatial -h boab.qaeco.com -p 5432 -U qaeco -w")
 
 plot(brt.preds, col=sdm.colors(100)) #Plot prediction map using red to white color scheme
 
