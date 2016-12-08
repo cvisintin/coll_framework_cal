313c707b2906c3b134cad37acbd36d89dedf97a4
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-12-09 10:48:34 +1100

    removed legend

--- R/autocorr_plots.R
+++ R/autocorr_plots.R
@@ -9,14 +9,14 @@ cal.cor.df <- cbind(cal.cor.df,"col"="deer")
 auto.resid <- rbind(cal.cor.df, vic.cor.df)
 
 tiff('figs/sac.tif', pointsize = 11, compression = "lzw", res=300, width = 1600, height = 900)
-ggplot(auto.resid,aes(x=x,y=y,group=col,shape=col)) +
+ggplot(auto.resid,aes(x=x,y=y,shape=col)) +
   geom_line(colour=c("grey70"),size=.3) +
   geom_point(size=1.0) +
   ylab("Moran's I") +
   xlab("Distance (km)") +
-  labs(shape = "Species") +
+  #labs(shape = "Species") +
   theme_bw() +
-  theme(legend.key = element_blank()) +
+  theme(legend.key = element_blank(), legend.position="none") +
   #scale_shape_manual(values=shapes) +
   geom_hline(aes(yintercept=0), linetype=2, size=0.3) +
   theme(text = element_text(size = 10)) +
