00b9b862f7f387c4b9e8848967c68ce4046dad6a
Casey Visintin <cvisintin@student.unimelb.edu.au>
2017-02-28 19:38:24 +1100

    added plots for completion talk

--- R/cal_collision_plots.R
+++ R/cal_collision_plots.R
@@ -182,3 +182,81 @@ ggplot(tspd, aes(x=x*1.6,y=y)) +
   #scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
 #guides(colour=FALSE)
 dev.off()
+
+###### alternative based on predictions for phd talk######
+
+occ.range <- seq(0,1,1/(nrow(data)+1))[-c(1,length(seq(0,1,1/(nrow(data)+1))))]
+
+occ.fit <- predict.glm(coll.glm,data.frame(deer=occ.range,tvol=mean(data$tvol),tspd=mean(data$tspd),length=1),type="response",se.fit=TRUE)
+occ <- data.frame(x=occ.range,y=occ.fit[["fit"]],ymin=occ.fit[["fit"]]-1.96*occ.fit[["se.fit"]],ymax=occ.fit[["fit"]]+1.96*occ.fit[["se.fit"]])
+
+png('/home/casey/Research/Projects/PhD_Thesis/completion_talk/graphics/cal_occ.png', pointsize = 16, res=300, width = 1000, height = 900)
+ggplot(occ, aes(x=x,y=y,ymin=ymin,ymax=ymax)) +
+  geom_line(size=0.8, colour='#1e501f') +
+  geom_ribbon(alpha=0.3) +
+  ylab("RELATIVE COLLISION RATE") +
+  xlab("LIKELIHOOD OF SPECIES OCCURRENCE") +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(plot.margin=unit(c(.5,.5,.1,.1),"cm")) +
+  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+  theme(text = element_text(size = 8)) +
+  scale_x_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1))
+dev.off()
+
+
+# tvol <- foreach(i = 1:length(models1000), .combine=rbind) %do% {
+#   data.frame(x=models1000[[i]][["data"]][["tvol"]], y=invcloglog(cbind(1,mean(log(models1000[[i]][["data"]][["deer"]])),log(models1000[[i]][["data"]][["tvol"]]),(log(models1000[[i]][["data"]][["tvol"]]))*(log(models1000[[i]][["data"]][["tvol"]])),mean(log(models1000[[i]][["data"]][["tspd"]]))) %*% models1000[[i]][["coefs"]]), col=rep(i, each=length(models1000[[i]][["data"]][["tvol"]])))
+# }
+
+tvol.range <- seq(0,40000,40000/(nrow(data)+1))[-c(1,length(seq(0,40000,40000/(nrow(data)+1))))]
+
+tvol.fit <- predict.glm(coll.glm,data.frame(deer=mean(data$deer),tvol=tvol.range,tspd=mean(data$tspd),length=1),type="response",se.fit=TRUE)
+tvol <- data.frame(x=tvol.range,y=tvol.fit[["fit"]],ymin=tvol.fit[["fit"]]-1.96*tvol.fit[["se.fit"]],ymax=tvol.fit[["fit"]]+1.96*tvol.fit[["se.fit"]])
+
+png('/home/casey/Research/Projects/PhD_Thesis/completion_talk/graphics/cal_tvol.png', pointsize = 16, res=300, width = 1000, height = 900)
+ggplot(tvol, aes(x=x/1000,y=y,ymin=ymin,ymax=ymax)) +
+  geom_line(size=0.8, colour='#1e501f') +
+  geom_ribbon(alpha=0.3) +
+  ylab("RELATIVE COLLISION RATE") +
+  xlab("TRAFFIC VOLUME (1000 VEHICLES/DAY)") +
+  #theme(legend.position="none") +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(plot.margin=unit(c(.5,.5,.1,.1),"cm")) +
+  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+  theme(text = element_text(size = 8)) +
+  scale_x_continuous(breaks=seq(0,40,by=5), expand = c(0, 0), lim=c(0,40))
+dev.off()
+
+
+# tspd <- foreach(i = 1:length(models1000), .combine=rbind) %do% {
+#   data.frame(x=models1000[[i]][["data"]][["tspd"]], y=invcloglog(cbind(1,mean(log(models1000[[i]][["data"]][["deer"]])),mean(log(models1000[[i]][["data"]][["tvol"]])),mean((log(models1000[[i]][["data"]][["tvol"]]))*(log(models1000[[i]][["data"]][["tvol"]]))),log(models1000[[i]][["data"]][["tspd"]])) %*% models1000[[i]][["coefs"]]), col=rep(i, each=length(models1000[[i]][["data"]][["tspd"]])))
+# }
+
+tspd.range <- seq(0,68.75,68.75/(nrow(data)+1))[-c(1,length(seq(0,68.75,68.75/(nrow(data)+1))))]
+
+tspd.fit <- predict.glm(coll.glm,data.frame(deer=mean(data$deer),tvol=mean(data$tvol),tspd=tspd.range,length=1),type="response",se.fit=TRUE)
+tspd <- data.frame(x=tspd.range,y=tspd.fit[["fit"]],ymin=tspd.fit[["fit"]]-1.96*tspd.fit[["se.fit"]],ymax=tspd.fit[["fit"]]+1.96*tspd.fit[["se.fit"]])
+
+png('/home/casey/Research/Projects/PhD_Thesis/completion_talk/graphics/cal_tspd.png', pointsize = 16, res=300, width = 1000, height = 900)
+ggplot(tspd, aes(x=x*1.6,y=y,ymin=ymin,ymax=ymax)) +
+  geom_line(size=0.8, colour='#1e501f') +
+  geom_ribbon(alpha=0.3) +
+  ylab("RELATIVE COLLISION RATE") +
+  xlab("TRAFFIC SPEED (KM/HOUR)") +
+  #theme(legend.position="none") +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(plot.margin=unit(c(.5,.5,.1,.1),"cm")) +
+  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+  theme(text = element_text(size = 8)) +
+  scale_x_continuous(breaks=seq(0,110,by=10), expand = c(0, 0), lim=c(0,110))
+dev.off()
+#############################################
