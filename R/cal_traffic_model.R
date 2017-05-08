ae1f757dc8a950fb118b8e8fbf47ae45d97aafd9
Casey Visintin <cvisintin@student.unimelb.edu.au>
2017-05-08 15:27:37 +1000

    added database write

--- R/cal_traffic_model.R
+++ R/cal_traffic_model.R
@@ -1,5 +1,10 @@
 require(randomForest)
 require(data.table)
+require(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")  #Specify a driver for postgreSQL type database
+con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres15", host="boab.qaeco.com", port="5432")  #Connection to database server on Boab
+
 
 model.data <- as.data.table(read.delim("data/cal_model_data_traffic.csv", header=T, sep=","))  #Read in traffic volume data for road segments
 
@@ -25,6 +30,9 @@ perf.vol <- merge(vol.preds.dt,model.data[!is.na(model.data$aadt),])
 plot(perf.vol$aadt,perf.vol$tvol)
 abline(a=0,b=.5, lty=2)
 
+dbWriteTable(con, c("gis_california", "cal_nogeom_roads_volpreds"), value = vol.preds.dt, row.names=FALSE, overwrite=TRUE)
+
+
 set.seed(123)
 speed.rf <- randomForest(formula = speedlmt ~ rdclass + rddens, data = model.data[!is.na(model.data$speedlmt),], mtry=2, importance = TRUE, sampsize = 1000)  #Fit random forest model
 
@@ -40,3 +48,5 @@ setkey(speed.preds.dt,uid)
 perf.spd <- merge(speed.preds.dt,model.data[!is.na(model.data$speedlmt),])
 plot(perf.spd$speedlmt,perf.spd$tspd)
 abline(a=0,b=.5, lty=2)
+
+dbWriteTable(con, c("gis_california", "cal_nogeom_roads_speedpreds"), value = speed.preds.dt, row.names=FALSE, overwrite=TRUE)
